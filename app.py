61515427da426cce855200b64fdbc3d9c45fddfb
carsonwolber <ctw54@cornell.edu>
2025-08-19 20:23:55 -0400

    patched check mthd

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import json
+import requests
 from flask_mail import Mail, Message
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
@@ -23,27 +24,27 @@ def check_for_opening():
   # see: https://classes.cornell.edu/content/FA25/api-details
   url = "https://classes.cornell.edu/api/2.0/search/classes.json"
   # PE1446 11:30 
-  crseId = 10155
+  classNbr = 10155
   params = {
       'roster': 'FA25',
       'subject': 'PE'
   }
 
   try:
-    response = request.get(url, params=params)
+    response = requests.get(url, params=params)
     data = response.json()
     all_classes = data.get('data', {}).get('classes', [])
         
     tennis_data = None
     for cls in all_classes:
-        if cls.get('crseId') == crseId:
-            tennis_data = cls
-            break
+      for enrollGroup in cls.get('enrollGroups', []):
+        for classSection in enrollGroup.get('classSections', []):
+                if classSection.get('classNbr') == classNbr:
+                  tennis_data = classSection
+                  break
     if tennis_data: 
-       if tennis_data["openStatus"] == "O":
-          send_open_notif()
-
-
+      if tennis_data["openStatus"] == "O":
+        send_open_notif()
   except Exception as e:
     raise e
 
@@ -51,3 +52,12 @@ def check_for_opening():
 
 def send_open_notif():
    pass
+
+def create_app():
+   return app
+
+if __name__ =='__main__':
+   app = create_app()
+   with app.app_context():
+      check_for_opening()
+      app.run(port=3000)
